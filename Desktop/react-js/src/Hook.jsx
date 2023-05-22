123ea6c540592daa79062770f3a86f1df772e825
Muhammad <121952755+myusuf4@users.noreply.github.com>
2023-05-22 22:11:36 +0500

    +5 -5

--- Desktop/react-js/src/Hook.jsx
+++ Desktop/react-js/src/Hook.jsx
@@ -8,8 +8,8 @@ const Hooks=()=>  {
         <div style={{flex:1}}>
             <h1>Hello Hooks</h1>
             <h1>Count:{count}</h1>
-            <button onClick={()=>setCount(count+1)}>+</button>
-            <button onClick={()=>setCount(count-1)}>-</button>
+            <button onClick={()=>setCount(count+5)}>+5</button>
+            <button onClick={()=>setCount(count-5)}>-5</button>
         </div>
     )
 
